46724271b39dc8b046cc8e4f0c9ced92cf349f50
Tyler Rollins <tylerrollins@outlook.com>
2014-02-19 14:28:12 +0000

    added students to hash and cohort

--- directory.rb
+++ directory.rb
@@ -1,38 +1,38 @@
 
 students = [
-  "Mario Gintili",
-  "Mikhail Dubov",
-  "Karolis Noreika",
-  "Michael Sidon",
-  "Charles De Barros",
-  "Ruslan Vikhor",
-  "Toby Retallick",
-  "Mark Mekhaiel",
-  "Sarah Young",
-  "Hannah Wight",
-  "Khushkaran Singh",
-  "Rick brunstedt",
-  "Manjit Singh",
-  "Ross Hepburn",
-  "Tiffanie Chia",
-  "Matthew Thomas",
-  "Freddy McGroarty",
-  "Tyler Rollins",
-  "Richard Curteis",
-  "Anna Yanova",
-  "Andrew Cumine"
+  {:name => "Mario Gintili", :cohort => :February},
+  {:name => "Mikhail Dubov", :cohort => :February},
+  {:name => "Karolis Noreika", :cohort => :February},
+  {:name => "Michael Sidon", :cohort => :February},
+  {:name => "Charles De Barros", :cohort => :February},
+  {:name => "Ruslan Vikhor", :cohort => :February},
+  {:name => "Toby Retallick", :cohort => :February},
+  {:name => "Mark Mekhaiel", :cohort => :February},
+  {:name => "Sarah Young", :cohort => :February},
+  {:name => "Hannah Wight", :cohort => :February},
+  {:name => "Khushkaran Singh", :cohort => :February},
+  {:name => "Rick brunstedt", :cohort => :February},
+  {:name => "Manjit Singh", :cohort => :February},
+  {:name => "Ross Hepburn", :cohort => :February},
+  {:name => "Tiffanie Chia", :cohort => :February},
+  {:name => "Matthew Thomas", :cohort => :February},
+  {:name => "Freddy McGroarty", :cohort => :February},
+  {:name => "Tyler Rollins", :cohort => :February}, 
+  {:name => "Richard Curteis", :cohort => :February}, 
+  {:name => "Anna Yanova", :cohort => :February},
+  {:name => "Andrew Cumine", :cohort => :February}
 ]
 def header
 puts "Student Directory - My Makers Academy Cohort"
 puts "-------------------"
 end
 
-def print(names)
-names.each { |name| puts name }
+def print(students)
+students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
 end
 
-def footer(names)
-puts "Overall, there are #{names.length} awesome students."
+def footer(students)
+puts "Overall, there are #{students.length} awesome students."
 end
 
 header
