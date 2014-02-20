e39b4a9811b7ddca247c553c86ff99688b7a6f47
Tyler Rollins <tylerrollins@outlook.com>
2014-02-20 13:37:48 +0000

    only selects students that start with A

--- directory.rb
+++ directory.rb
@@ -4,8 +4,16 @@ print "Student Directory - My Makers Academy Cohort\n"
 print "-------------------\n"
 end
 
+def student_a(student)
+	student[:name][0] == "a".capitalize!
+end
+
 def show(students)
-students.each_with_index { |student, index| print "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" }
+students.each_with_index do |student, index| 
+	if student_a(student) 
+		print  "#{index + 1}. #{student[:name]} if student (#{student[:cohort]} cohort)\n"
+	end
+end
 end
 
 def footer(students)
@@ -19,14 +27,14 @@ def input_students
 	#create empty array
 	students = []
 	#get name
-	name = gets.chomp
+	name = gets.chomp.capitalize
 	#while name is not empty, repeat
 	while !name.empty? do
 		#add hash to empty array
 		students << {:name => name, :cohort => :February}
 		print "Now we have #{students.length} students\n"
 		#get another name
-		name = gets.chomp
+		name = gets.chomp.capitalize
 	end
 	#return array students
 	students
