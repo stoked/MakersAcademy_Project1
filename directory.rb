b4d65cbae598dae330cae56c4040f0689f46d07a
Tyler Rollins <tylerrollins@outlook.com>
2014-02-21 13:13:29 +0000

    added default values

--- directory.rb
+++ directory.rb
@@ -1,7 +1,7 @@
 
 def header
-puts "Student Directory - My Makers Academy Cohort"
-puts "-------------------"
+  puts "Student Directory - My Makers Academy Cohort"
+  puts "-------------------"
 end
 
 def input_students
@@ -13,23 +13,27 @@ def input_students
 	puts "Where are you from?"
 	country = gets.chomp.upcase
 	
-	while !name.empty? do
-		students << {:name => name, :lastname => lastname, :country => country, :cohort => :February}
-		puts "Now we have #{students.length} students"
-		name = gets.chomp.capitalize
+	name = "NA".to_sym if name.empty?
+	lastname = "NA".to_sym if lastname.empty?
+	country = "NA".ty_sym if country.empty?
 
-	puts "Would you like to add another student? Yes/No"
-	another = gets.chomp.downcase
+	while !name.empty? do
+      students << {:name => name, :lastname => lastname, :country => country, :cohort => :February}
+	  puts "Now we have #{students.length} students"
+	  name = gets.chomp.capitalize
+
+	  puts "Would you like to add another student? Yes/No"
+	  another = gets.chomp.downcase
 	if another == "yes"
-  puts "Please enter first name?"
-	name = gets.chomp.capitalize
-	puts "Please eneter your last name?"
-	lastname = gets.chomp.capitalize
-	puts "Where are you from?"
-	country = gets.chomp.upcase
+      puts "Please enter first name?"
+	  name = gets.chomp.capitalize
+	  puts "Please eneter your last name?"
+	  lastname = gets.chomp.capitalize
+	  puts "Where are you from?"
+	  country = gets.chomp.upcase
 	else 
 		break
-end
+    end
 end
 students
 end
@@ -41,7 +45,7 @@ def show(students)
 end
 
 def footer(students)
-puts "Overall, there are #{students.length} awesome students."
+  puts "Overall, there are #{students.length} awesome students."
 end
 
 students = input_students
